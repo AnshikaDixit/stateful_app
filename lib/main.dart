5d9dffc58b605e1d8ee34d1b5d3742836413a114
anshikadixit09 <anshika.dixit@dianapps.com>
2023-01-24 11:30:31 +0530

    homepage redrawn usinf valuekey

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
@@ -8,7 +9,7 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: ApiProvider(api: Api(), child: const HomePage()),
     ),
   );
 }
@@ -21,23 +22,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String title = 'Tap the Screen';
+  ValueKey _textKey = const ValueKey<String?>(null);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(ApiProvider.of(context).api.dateAndTime ?? ''),
       ),
       body: GestureDetector(
-        onTap: (() {
+        onTap: () async {
+          final api = ApiProvider.of(context).api;
+          final dateAndTime = await api.getDateAndTime();
           setState(() {
             //it cannot be null
-            title = DateTime.now().toIso8601String();
+            //title = DateTime.now().toIso8601String();
+            _textKey = ValueKey(dateAndTime);
           });
-        }),
-        child: Container(color: Colors.white),
+        },
+        child: SizedBox.expand(
+          child: Container(
+            color: Colors.white,
+            child: DateTimeWidget(key: _textKey),
+            )),
       ),
     );
   }
 }
+
+class DateTimeWidget extends StatelessWidget {
+  const DateTimeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final api = ApiProvider.of(context).api;
+    return Text(
+      api.dateAndTime ?? 'Tap on screen to fetch date and time',
+    );
+  }
+}
+
+//We will display the current time in our Scaffold app bar and in a text widget independently
+
+//API provider can wrap our homepage so that it is available for all its descendants
+
+
+ class Api{
+  //provider class that inherits from InheritedWidget
+  //a way to check whether the inherited class is completely been replaced  - uuid
+
+  String? dateAndTime;
+  getDateAndTime() {
+    return Future.delayed(
+      const Duration(seconds: 1),
+      () => DateTime.now().toIso8601String(),
+    ).then((value) {
+      dateAndTime = value;
+      return value;
+    });
+  }
+}
+class ApiProvider extends InheritedWidget {
+  final Api api;
+  final String uuid;
+
+  ApiProvider({Key? key, required this.api, required Widget child})
+      : uuid = const Uuid().v4(),
+        super(child: child, key: key);
+
+  @override
+  bool updateShouldNotify(covariant ApiProvider oldWidget) {
+    return uuid != oldWidget.uuid;
+  }
+
+  static ApiProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ApiProvider>()!;
+  }
+  
+ }
